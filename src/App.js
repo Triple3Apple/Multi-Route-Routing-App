81ba8d57a50133001fef7d2d5f4bbbdc08731384
Abrar Akhand <Jahin345@gmail.com>
2021-06-25 23:01:40 -0700

    Set up React and added map to webpage

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
+import { useEffect, useRef, useState } from 'react';
 import './App.css';
+import * as tt from '@tomtom-international/web-sdk-maps'
 
-function App() {
+const App = () => {
+  const mapElement = useRef();
+  const [map, setMap] = useState({});
+
+  // Hard coded for now
+  // 33.42153444499368, -111.93985876629507
+
+  // const latitude = 33.42153444499368;
+  // const longitude = -111.93985876629507;
+
+  const latitude = 33.42153444499368;
+  const longitude = -111.93985876629507;
+
+  useEffect(() => {
+
+    // https://developer.tomtom.com/blog/build-different/tilt-and-rotation-tomtom-javascript-maps-sdk-web-v6
+    let map = tt.map({
+      // REMEMBER!!!! for React, env variables need to have the REACT_APP prefix!!!!!
+      key: process.env.REACT_APP_TOM_TOM_KEY,
+      container: mapElement.current,
+      center: { lat: latitude, lng: longitude },
+      zoom: 13,
+    });
+
+    setMap(map);
+
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div ref={mapElement} className='map'></div>
     </div>
   );
 }
