23d0da1b3d4ffec34a25f5ea9c3976153c36f401
Abrar Akhand <Jahin345@gmail.com>
2021-06-27 12:22:40 -0700

    Basic Routing implemented

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
-import { useEffect, useRef, useState } from 'react';
+
+
 import './App.css';
-import * as tt from '@tomtom-international/web-sdk-maps'
+// The css below makes the marker appear correctly.
+import '@tomtom-international/web-sdk-maps/dist/maps.css';
+
+import { useEffect, useRef, useState } from 'react';
+import * as ttm from '@tomtom-international/web-sdk-maps';
+import * as tts from '@tomtom-international/web-sdk-services';
 
 
 
@@ -19,10 +25,56 @@ const App = () => {
   // const latitude = 33.42153444499368;
   // const longitude = -111.93985876629507;
 
+  // Function that gets lngLat and converts to points.
+  const convertToPoints = (lngLat) => {
+    return {
+      point: {
+        latitude: lngLat.lat,
+        longitude: lngLat.lng,
+      }
+    }
+  }
+
+  const drawRoute = (geoJson, map) => {
+    if (map.getLayer('route')) {
+      map.removeLayer('route');
+      map.removeSource('route');
+    }
+    map.addLayer({
+      id: 'route',
+      type: 'line',
+      source: {
+        type: 'geojson',
+        data: geoJson,
+      },
+      paint: {
+        'line-color': 'red',
+        'line-width': 6,
+      }
+    })
+  }
+
+  const addDeliveryMarker = (lngLat, map) => {
+    const element = document.createElement('div');
+    element.className = 'marker-delivery';
+    new ttm.Marker({
+      element: element
+    })
+      .setLngLat(lngLat)
+      .addTo(map)
+  }
+
   useEffect(() => {
 
+    const origin = {
+      lat: latitude,
+      lng: longitude,
+    }
+
+    const destinations = [];
+
     // https://developer.tomtom.com/blog/build-different/tilt-and-rotation-tomtom-javascript-maps-sdk-web-v6
-    let map = tt.map({
+    let map = ttm.map({
       // REMEMBER!!!! for React, env variables need to have the REACT_APP prefix!!!!!
       key: process.env.REACT_APP_TOM_TOM_KEY,
       container: mapElement.current,
@@ -38,18 +90,95 @@ const App = () => {
 
     const addMarker = () => {
 
+      // Where the popup is placed relative to the marker
+      const popupOffset = {
+        bottom: [0, -30]
+      }
+
+      const popup = new ttm.Popup({ offset: popupOffset }).setHTML('This is you!');
+
       const element = document.createElement('div');
       element.className = 'marker';
 
-      const marker = new tt.Marker({
+      const marker = new ttm.Marker({
         draggable: true,
         element: element,
       })
         .setLngLat({ lat: latitude, lng: longitude })
         .addTo(map);
 
+
+      // Runs when marker has stopped being dragged.
+      marker.on('dragend', () => {
+        // Get new longitude and latitude.
+        const lngLat = marker.getLngLat();
+        setLatitude(lngLat.lat);
+        setLongitude(lngLat.lng);
+      })
+
+      marker.setPopup(popup).togglePopup();
+
     }
     addMarker()
+
+    const sortDestinations = (locations) => {
+      const pointsForDestinations = locations.map((destination) => {
+        return convertToPoints(destination)
+      })
+
+      const callParameters = {
+        key: process.env.REACT_APP_TOM_TOM_KEY,
+        destinations: pointsForDestinations,
+        origins: [convertToPoints(origin)],
+      }
+
+      return new Promise((resolve, reject) => {
+        tts.services
+          .matrixRouting(callParameters)
+          .then((matrixAPIResults) => {
+            const results = matrixAPIResults.matrix[0]
+            const resultsArray = results.map((result, index) => {
+              return {
+                location: locations[index],
+                drivingtime: result.response.routeSummary.travelTimeInSeconds,
+              }
+            });
+
+            resultsArray.sort((a, b) => {
+              return a.drivingtime - b.drivingtime;
+            });
+
+            const sortedLocations = resultsArray.map((result) => {
+              return result.location;
+            });
+
+            resolve(sortedLocations);
+
+          });
+      });
+    }
+
+    const recalculateRoutes = () => {
+      sortDestinations(destinations).then((sorted) => {
+        sorted.unshift(origin);
+
+        tts.services
+          .calculateRoute({
+            key: process.env.REACT_APP_TOM_TOM_KEY,
+            locations: sorted,
+          })
+          .then((routeData) => {
+            const geoJson = routeData.toGeoJson();
+            drawRoute(geoJson, map);
+          });
+      });
+    }
+
+    map.on('click', (e) => {
+      destinations.push(e.lngLat);
+      addDeliveryMarker(e.lngLat, map);
+      recalculateRoutes();
+    })
 
     return () => map.remove();
 
