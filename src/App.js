be41746beb8ab6dd9b5662db81b198a6aad04f13
Abrar Akhand <Jahin345@gmail.com>
2021-06-26 22:37:12 -0700

    Marker placed on map and added some styling

--- src/App.js
+++ src/App.js
@@ -2,9 +2,13 @@ import { useEffect, useRef, useState } from 'react';
 import './App.css';
 import * as tt from '@tomtom-international/web-sdk-maps'
 
+
+
 const App = () => {
   const mapElement = useRef();
   const [map, setMap] = useState({});
+  const [longitude, setLongitude] = useState(-0.112869);
+  const [latitude, setLatitude] = useState(51.504);
 
   // Hard coded for now
   // 33.42153444499368, -111.93985876629507
@@ -12,8 +16,8 @@ const App = () => {
   // const latitude = 33.42153444499368;
   // const longitude = -111.93985876629507;
 
-  const latitude = 33.42153444499368;
-  const longitude = -111.93985876629507;
+  // const latitude = 33.42153444499368;
+  // const longitude = -111.93985876629507;
 
   useEffect(() => {
 
@@ -22,18 +26,61 @@ const App = () => {
       // REMEMBER!!!! for React, env variables need to have the REACT_APP prefix!!!!!
       key: process.env.REACT_APP_TOM_TOM_KEY,
       container: mapElement.current,
+      stylesVisibility: {
+        trafficIncidents: true,
+        trafficFlow: true,
+      },
       center: { lat: latitude, lng: longitude },
       zoom: 13,
     });
 
     setMap(map);
 
-  }, []);
+    const addMarker = () => {
 
+      const element = document.createElement('div');
+      element.className = 'marker';
+
+      const marker = new tt.Marker({
+        draggable: true,
+        element: element,
+      })
+        .setLngLat({ lat: latitude, lng: longitude })
+        .addTo(map);
+
+    }
+    addMarker()
+
+    return () => map.remove();
+
+  }, [latitude, longitude]);
+
+  // html code below
+  // TODO: Fix problem with entering negative values (maybe add button that submits the 
+  // result, or some sort of verification?).
   return (
-    <div className="App">
-      <div ref={mapElement} className='map'></div>
-    </div>
+    <>
+      {map && <div className="app">
+        <div ref={mapElement} className='map'></div>
+        <div className="search-bar">
+          <h1>Where to?</h1>
+          <input
+            type="text"
+            name=""
+            id="latitude"
+            placeholder="Put in Latitude"
+            onChange={(e) => { setLatitude(e.target.value) }}
+          />
+          <input
+            type="text"
+            name=""
+            id="longitude"
+            placeholder="Put in Longitude"
+            onChange={(e) => { setLongitude(e.target.value) }}
+          />
+        </div>
+      </div>}
+    </>
   );
 }
 
